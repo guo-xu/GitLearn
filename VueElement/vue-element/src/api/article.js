d94c39c8004958d0fa8b0df812897b2400b3ab63
guoxu <iamguoxu@163.com>
2019-08-27 10:51:59 +0800

    add proxy

--- VueElement/vue-element/src/api/article.js
+++ VueElement/vue-element/src/api/article.js
@@ -1,11 +1,10 @@
-import base from './base'; //导入接口域名列表
 import axios from '@/utils/http';//导入http中创建的axios实例
 //import qs from 'qs';//根据需求是否导入qs模块
 
 const article = {
     //新闻列表
     articleList (params) {
-        return axios.get(`/toutiao/index`, {
+        return axios.get(`/api/toutiao/index`, {
             params: params
         });
     },
--- VueElement/vue-element/vue.config.js
+++ VueElement/vue-element/vue.config.js
@@ -7,6 +7,16 @@ module.exports = {
     // 指定生成的 index.html 的输出路径(相对于 outputDir)，也可以是一个绝对路径
     indexPath: 'index.html',
     devServer: {
-        proxy:'http://v.juhe.cn/'
+        proxy: {
+            //例如将路径 localhost:8080/api/xxx 代理到 http://v.juhe.cn/xxx
+            //如果不加pathRewrite路径重写， 将会代理到  http://v.juhe.cn/api/xxx
+            '/api': {
+                target: 'http://v.juhe.cn',
+                pathRewrite: {'^/api': ''},   //不想传递 /api，需要重写路径，将前缀 '/api' 转为 '/'
+                changeOrigin: true, //将主机标头的原点更改为目标url
+                ws: true, //是否代理 websockets
+                secure: false, // 是否验证SSL Certs,  https接口需要配置该参数
+            }
+        }
     }
 }
