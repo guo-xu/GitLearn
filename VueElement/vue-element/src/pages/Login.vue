ad3c24ac0bf847b2dfbc8e7ccec7f9a3efc8f479
guoxu <iamguoxu@163.com>
2019-09-02 10:04:01 +0800

    add login.html

---
+++ VueElement/vue-element/src/pages/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="loginDiv">
+        login
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    
+</style>
--- VueElement/vue-element/src/router/index.js
+++ VueElement/vue-element/src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '@/testPages/Home'
+import Login from '@/pages/Login'
+import Home from '@/pages/Home'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
     routes: [{
         path: '/',
+        name: 'Login',
+        component: Login
+    },{
+        path: '/home',
         name: 'Home',
         component: Home
     }]
